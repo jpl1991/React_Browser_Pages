935caa34d46174b67aece97aaec397ae743a622d
Peilin Jiang <peilinjiang@peilins-mbp.wireless.concordia.ca>
2018-04-07 18:00:38 -0400

    add semantic ui

--- bookworm-app/src/App.js
+++ bookworm-app/src/App.js
@@ -3,8 +3,9 @@ import {Route} from 'react-router-dom';
 import HomePage from './component/pages/HomePage';
 import LoginPage from './component/pages/LoginPage';
 
+
 const App =() => (
-  <div>
+  <div className = 'ui Container'>
     <Route path='/' exact component = {HomePage} />
 
     <Route path='/login' exact component = {LoginPage} />
